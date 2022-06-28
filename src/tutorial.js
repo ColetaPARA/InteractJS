9cc6feb0aba0a12df8aa20da85a3935ef4cfd396
JonathanSaless <jonathansales1998@gmail.com>
2022-06-27 22:12:59 -0300

    commit

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-const splash = document.querySelector('.splash');
-
-document.addEventListener('DOMContentLoaded', (e)=>{
-    setTimeout(()=>{
-        splash.classList.add('display-none');
-    }, 2000);
-=======
 const splash = document.querySelector('.splash');
 
 document.addEventListener('DOMContentLoaded', ()=>{
     setTimeout(()=>{
         splash.classList.add('display-none');
     }, 2000);
->>>>>>> Jonathan
 });
