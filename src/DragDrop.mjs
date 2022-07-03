cd8b2464b8582815a1ab9f03907ae056d6286391
JonathanSaless <jonathansales1998@gmail.com>
2022-07-03 00:52:39 -0300

    criacao de classe game e exclusao de arquivo desnecessario

--- src/DragDrop.mjs
+++ src/DragDrop.mjs
@@ -1,9 +1,11 @@
 import interact from "https://cdn.interactjs.io/v1.9.20/interactjs/index.js";
 // import interact from "interactjs";
 import { Score } from "./Score.mjs";
-import { createTrash } from "./game.js";
+import { Game } from "./game.js"
 
 const score = new Score("score");
+const gameplay = new Game();
+
 //const objetos = document.querySelector(".objects");
 /**
  * A partir de um id cria uma zona de drop.
@@ -17,7 +19,7 @@ function dropzone(trash_obj) {
       ondrop: (event) => {
         var draggableElement = event.relatedTarget;
         score.earn_score(1000, 1);
-        createTrash();
+        gameplay.createTrash();
         draggableElement.remove();
       },
     })
--- src/game.js
+++ src/game.js
@@ -1,78 +1,90 @@
-const lixos = document.querySelectorAll("div.lixo");
-const objetos = document.querySelector(".objects");
-var times = document.querySelector("#time");
-var timer = 0;
-lixos.forEach((lixo) => { //gera os lixos iniciais com tipos aleatoriamente escolhidos
-    let num = Math.floor(Math.random() * 5);
-    switch (num) {
-        case 0:
-            lixo.classList.add('objMetal')
-            break;
-        case 1:
-            lixo.classList.add('objPlastico')
-            break;
-        case 2:
-            lixo.classList.add('objPapel')
-            break;
-        case 3:
-            lixo.classList.add('objOrganico')
-            break;
-        case 4:
-            lixo.classList.add('objVidro')
-        break;
-    }
-});
+class Game {
+    constructor() {
+        this.lixos = document.querySelectorAll("div.lixo");
+        this.objetos = document.querySelector(".objects");
+        this.tempos = document.querySelector("#time");
+        this.pontos = 0;
+        this.tempo = 0;
 
-const myInterval = setInterval(() => {
-    if(timer < 1){
-        console.log(time);
-        timer ++;
-        times.innerHTML = timer;
+        this.start();
     }
-    else{       
-        console.log("fazer algo para parar o jogo");  
-        stopGame();
+
+    start() {
+        this.lixos.forEach((lixo) => { //gera os lixos iniciais com tipos aleatoriamente escolhidos
+            let num = Math.floor(Math.random() * 5);
+            switch (num) {
+                case 0:
+                    lixo.classList.add('objMetal')
+                    break;
+                case 1:
+                    lixo.classList.add('objPlastico')
+                    break;
+                case 2:
+                    lixo.classList.add('objPapel')
+                    break;
+                case 3:
+                    lixo.classList.add('objOrganico')
+                    break;
+                case 4:
+                    lixo.classList.add('objVidro')
+                break;
+            }
+        });
+        this.startTime();
+        this.createTrash()
     }
-}, 1000);
 
-function stopGame (){
-    //criar função que verifica se usuário perdeu
-    //criar função que verifica se usuário venceu
-    let corpoHtml = document.body;
-    let gameOver = document.createElement('div');
-    let butao = '';
-    gameOver.classList.add('gameover');
-    gameOver.innerHTML = "GAME OVER </br>SCORE: {}<button onclick='location.href='/menu';'>GO</button>";
-    corpoHtml.appendChild(gameOver);
-    clearInterval(myInterval);
-    document.addEventListener('oninput', ()=>{
-        console.log('clicou na tela');
-        
-    });
-}
-
-export function chooseTrash(){
-    let num = Math.floor(Math.random() * 5);
-    switch (num) {
-        case 0:
-            return 'lixo objMetal';
-        case 1:
-            return 'lixo objPlastico';
-        case 2:
-            return 'lixo objPapel';
-        case 3:
-            return 'lixo objOrganico';
-        case 4:
-            return 'lixo objVidro';
+    startTime(){
+        var myInterval = setInterval(() => {
+            if(this.tempo < 60){
+                console.log(time);
+                this.tempo ++;
+                this.tempos.innerHTML = this.tempo;
+            }
+            else{       
+                console.log("fazer algo para parar o jogo");  
+                this.stop(myInterval);
+            }
+        }, 1000);
     }
-}
 
-export function createTrash(){
-    setInterval(() => {
-        if(document.querySelectorAll("div.lixo").length < 5){ //verifica se tem menos de 5 lixos na tela
-          let trash = document.createElement("div");
-          trash.classList = chooseTrash();
-          objetos.appendChild(trash);
+    stop(Interval) {
+        //criar função que verifica se usuário perdeu
+        //criar função que verifica se usuário venceu
+        let corpoHtml = document.body;
+        let gameOver = document.createElement('div');      
+        gameOver.classList.add('gameover');
+        gameOver.innerHTML = "GAME OVER </br>SCORE: {}<button onclick='location.href='/menu';'>GO</button>";
+        corpoHtml.appendChild(gameOver);
+        clearInterval(Interval);
+    }
+
+    chooseTrash(){
+        let num = Math.floor(Math.random() * 5);
+        switch (num) {
+            case 0:
+                return 'lixo objMetal';
+            case 1:
+                return 'lixo objPlastico';
+            case 2:
+                return 'lixo objPapel';
+            case 3:
+                return 'lixo objOrganico';
+            case 4:
+                return 'lixo objVidro';
         }
-      }, 3000)
+    }
+
+    createTrash(){
+        setInterval(() => {
+            if(document.querySelectorAll("div.lixo").length < 5){ //verifica se tem menos de 5 lixos na tela
+              let trash = document.createElement("div");
+              trash.classList = this.chooseTrash();
+              this.objetos.appendChild(trash);
+            }
+          }, 3000)
+    }
+    
 }
+
+export { Game };
